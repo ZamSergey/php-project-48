980416f5e9eba23a738fa87fae3cef08379023e2
ZamSergey <zamsergey@gmail.com>
2025-04-07 22:19:42 +0300

    Плоский поиск различий

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ZamSergey/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ZamSergey/php-project-48/actions)
+
+# Пример использования Вычислителя различий через консоль
+https://asciinema.org/a/uiyig8NkT3h1dMVdlpMowxgsa
--- src/FileParser.php
+++ src/FileParser.php
@@ -2,11 +2,43 @@
 
 namespace GenDiff\FileParser;
 
-function parseFile($fileSrc)
+function genDiff($file1, $file2)
 {
+    $key1 = (parseFile($file1));
+    $key2 = (parseFile($file2));
 
+    $keys = array_merge($key1,$key2);
+    ksort($keys);
+   
+    $result = '';
+    
+    foreach($keys as $key => $val) {
+        $difString = '';
+        if(array_key_exists($key,$key1) && array_key_exists($key,$key2) && $key1[$key] === $key2[$key]) {
+            $difString = "  $key: $val \r\n";
+        }
+        elseif(array_key_exists($key,$key1) && array_key_exists($key,$key2) && $key1[$key] !== $key2[$key]) {
+            $difString = "- $key: {$key1[$key]}\r\n";
+            $difString .="    + $key: $key2[$key]\r\n";
+        }
+        elseif(array_key_exists($key,$key1) && !array_key_exists($key,$key2)) {
+            $difString = "- $key: {$key1[$key]}\r\n";
+        }
+        elseif(!array_key_exists($key,$key1) && array_key_exists($key,$key2)) {
+            $difString = "+ $key: {$key2[$key]}\r\n";
+        }
+        $result .= "    $difString";
+    }
+
+    echo "{\r\n$result}\r\n";
+   
+}
+
+function parseFile(String $fileSrc) : Array
+{
     $fileContent = file_get_contents($fileSrc, FILE_USE_INCLUDE_PATH);
     $data = json_decode($fileContent);
-    $keys = (get_object_vars($data));
-    print_r($keys);
+    
+    $keys = get_object_vars($data);    
+    return $keys;
 }
